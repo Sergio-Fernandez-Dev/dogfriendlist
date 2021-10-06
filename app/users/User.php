c9bf701a3d295e65d8124cdc37d8de356fe36449
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-06 19:50:39 +0200

    User model

---
+++ app/users/User.php
@@ -0,0 +1,142 @@
+<?php 
+
+namespace App\Users\User;
+
+class User
+{
+    private $id;
+    private $nickname;
+    private $email;
+    private $visibility;
+    private $city;
+    private $country;
+    private $name;
+    private $surname;
+    private $img;
+    private $aboutMe;
+    private $passHash;
+    private $createdAt;
+
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNickname()
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname($nickname)
+    {
+        $this->nickname = $nickname;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getVisibility()
+    {
+        return $this->visibility;
+    }
+
+    public function setVisibility($visibility)
+    {
+        $this->visibility = $visibility;
+    }
+
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+ 
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    public function setImg($img)
+    {
+        $this->img = $img;
+    }
+
+    public function getAboutMe()
+    {
+        return $this->aboutMe;
+    }
+
+    public function setAboutMe($aboutMe)
+    {
+        $this->aboutMe = $aboutMe;
+    }
+
+    public function getPassHash()
+    {
+        return $this->passHash;
+    }
+ 
+    public function setPassHash($passHash)
+    {
+        $this->passHash = $passHash;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+}
+
+?>
