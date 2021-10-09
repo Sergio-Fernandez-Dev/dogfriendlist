4db45a0a81d82a025d2bf739c3bf16c0dcebc0a3
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-09 21:29:51 +0200

    Changing interface name

---
+++ app/users/interfaces/UserDataMapperInterface.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\Interfaces;
+
+use App\Users\User;
+
+interface UserDataMapperInterface
+{
+    public function find($param);
+    public function insert(User $user);
+    public function update(User $user);
+    public function delete(User $user);
+}
+
+?>
