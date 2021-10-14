b7c37abd858b51d83ad572e2d287c6f696651f61
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-15 01:00:08 +0200

    Removing default argument values

--- app/core/interfaces/QueryBuilderInterface.php
+++ app/core/interfaces/QueryBuilderInterface.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types = 1);
+<?php 
 
 namespace App\Core\Interfaces;
 
@@ -8,26 +8,26 @@ interface QueryBuilderInterface
 {
     public function sql(string $query): QueryBuilder;
 
-    public function insert(string $table, array $values, ?array $fields = NULL): QueryBuilder;
-    public function select(array $fields = null): QueryBuilder;
-    public function selectDistinct(array $fields): QueryBuilder;
+    public function insert(string $table, array $values, ?array $fields): QueryBuilder;
+    public function select(?array $fields): QueryBuilder;
+    public function selectDistinct(?array $fields): QueryBuilder;
     public function update(string $table): QueryBuilder;
     public function delete(string $table): QueryBuilder;
 
-    public function set (string $field, string $operator = '=', string $value): QueryBuilder;
+    public function set (string $field, string $operator, string $value): QueryBuilder;
     public function as(string $alias): QueryBuilder;
     public function from(string $table): QueryBuilder;
 
-    public function where(string $field, string $operator = '=', string $value): QueryBuilder;
-    public function whereAnd(string $field, string $operator = '=', string $value): QueryBuilder;
-    public function whereOr(string $field, string $operator = '=', string $value): QueryBuilder;
-    public function whereNot(string $field, string $operator = '=', string $value): QueryBuilder;
-    public function whereIsNull(bool $value = true): QueryBuilder;
+    public function where(string $field, string $operator, string $value): QueryBuilder;
+    public function whereAnd(string $field, string $operator, string $value): QueryBuilder;
+    public function whereOr(string $field, string $operator, string $value): QueryBuilder;
+    public function whereNot(string $field, string $operator, string $value): QueryBuilder;
+    public function whereIsNull(bool $value): QueryBuilder;
 
     public function limit(int $start, int $offset): QueryBuilder;
-    public function orderBy(string $field, string $order = 'ASC'): QueryBuilder;
-    public function join(string $type = 'INNER', string $table): QueryBuilder;
-    public function on(string $field, string $operator = '=', string $value): QueryBuilder;
+    public function orderBy(string $field, string $order): QueryBuilder;
+    public function join(string $type, string $table): QueryBuilder;
+    public function on(string $field, string $operator, string $value): QueryBuilder;
 
     public function get(): string;
 }
