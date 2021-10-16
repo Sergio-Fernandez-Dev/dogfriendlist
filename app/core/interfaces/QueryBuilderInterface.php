8c6391158ca8a510eb18ea88d38decc33a6cc5e1
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-16 12:33:07 +0200

    Removing isNull()

--- app/core/interfaces/QueryBuilderInterface.php
+++ app/core/interfaces/QueryBuilderInterface.php
@@ -9,8 +9,8 @@ interface QueryBuilderInterface
     public function sql(string $query): QueryBuilder;
 
     public function insert(string $table, array $values, ?array $fields): QueryBuilder;
-    public function select(?array $fields): QueryBuilder;
-    public function selectDistinct(?array $fields): QueryBuilder;
+    public function select(array $fields): QueryBuilder;
+    public function selectDistinct(array $fields): QueryBuilder;
     public function update(string $table): QueryBuilder;
     public function delete(string $table): QueryBuilder;
 
@@ -19,17 +19,16 @@ interface QueryBuilderInterface
     public function from(string $table): QueryBuilder;
 
     public function where(string $field, string $operator, string $value): QueryBuilder;
-    public function whereAnd(string $field, string $operator, string $value): QueryBuilder;
-    public function whereOr(string $field, string $operator, string $value): QueryBuilder;
+    public function andWhere(string $field, string $operator, string $value): QueryBuilder;
+    public function orWhere(string $field, string $operator, string $value): QueryBuilder;
     public function whereNot(string $field, string $operator, string $value): QueryBuilder;
-    public function whereIsNull(bool $value): QueryBuilder;
 
     public function limit(int $start, int $offset): QueryBuilder;
     public function orderBy(string $field, string $order): QueryBuilder;
-    public function join(string $type, string $table): QueryBuilder;
+    public function join(string $table, string $type): QueryBuilder;
     public function on(string $field, string $operator, string $value): QueryBuilder;
-
-    public function get(): string;
+    
+    public function get(): string; 
 }
 
-?>
+?>           
