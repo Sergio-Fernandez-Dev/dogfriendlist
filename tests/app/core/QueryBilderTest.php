08f4295c42f82a000fcd8dd701016d9122fc6386
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-16 12:35:18 +0200

    Adding new tests

--- tests/app/core/QueryBilderTest.php
+++ tests/app/core/QueryBilderTest.php
@@ -151,7 +151,7 @@ class QueryBuilderTest extends TestCase
         $this->assertEquals($expected, $query);      
     }
 
-    public function testFromReturnsExceptionIfQueryTypeIsNotSelect()
+    public function testFromReturnsExceptionIfQueryTypeIsNotSet()
     {
         $this->expectException(Exception::class);
         $this->qb->insert('Users', ['Germany', 'Berlin'])
@@ -169,6 +169,226 @@ class QueryBuilderTest extends TestCase
             
         $this->assertEquals($expected, $query);    
     }
+
+    public function testWhereReturnsExceptionIfQueryTypeIsNotSet()
+    {
+        $this->expectException(Exception::class);
+        $this->qb->where('Country','=','Germany')
+                ->get();
+    }
+
+    public function testWhereReturnsExceptionIfQueryTypeIsWrong()
+    {
+        $this->expectException(Exception::class);
+        $this->qb->insert('Users', ['Germany', 'Berlin'])
+                ->where('Country','=','Germany')
+                ->get();
+    }
+
+    public function testWhereReturnsExceptionIfTwoWhereCommandsAreSet()
+    {
+        $this->expectException(Exception::class);
+        $query = $this->qb->select(['Country'])
+                        ->as('Homeland')
+                        ->from('Users')
+                        ->where('City','=','Berlin')
+                        ->where('Name','=','Hannsel')
+                        ->get();
+    }
+
+    public function testTwoWhereCommandsReturnsExpectedSqlStringIfJoinCommandIsSet()
+    {
+        $expected = "SELECT Country AS Homeland FROM Users WHERE City = 'Berlin' INNER JOIN Spots".";";
+        $query = $this->qb->select(['Country'])
+                        ->as('Homeland')
+                        ->from('Users')
+                        ->where('City','=','Berlin')
+                        ->join('Spots')
+                        ->get();
+            
+        $this->assertEquals($expected, $query);    
+    }  
+
+    public function testWhereReturnsExpectedSqlStringIfIsnullIsGiven()
+    {
+        $expected = "SELECT Country AS Homeland FROM Users WHERE City IS NULL".";";
+        $query = $this->qb->select(['Country'])
+                        ->as('Homeland')
+                        ->from('Users')
+                        ->where('City','=','is null')
+                        ->get();
+            
+        $this->assertEquals($expected, $query);    
+    }  
+    
+    public function testWhereReturnsExpectedSqlString()
+    {
+        $expected = "SELECT Country AS Homeland FROM Users WHERE City = 'Berlin'".";";
+        $query = $this->qb->select(['Country'])
+                        ->as('Homeland')
+                        ->from('Users')
+                        ->where('City','=','Berlin')
+                        ->get();
+            
+        $this->assertEquals($expected, $query);    
+    }  
+    
+    public function testAndwhereReturnsExceptionIfQueryTypeIsNotSet()
+    {
+        $this->expectException(Exception::class);
+        $this->qb->as('Homeland')
+                ->from('Users')
+                ->where('City','=','Berlin')
+                ->andWhere('Name','=','Hannsel')
+                ->get();
+    }
+
+    public function testAndwhereReturnsExceptionIfQueryTypeIsWrong()
+    {
+        $this->expectException(Exception::class);
+        $this->qb->insert('Users', ['Germany', 'Berlin'])
+                ->as('Homeland')
+                ->from('Users')
+                ->where('City','=','Berlin')
+                ->andWhere('Name','=','Hannsel')
+                ->get();
+    }
+
+    public function testAndwhereReturnsExceptionIfWhereIsNotSet()
+    {
+        $this->expectException(Exception::class);
+        $this->qb->select(['Country'])
+                ->as('Homeland')
+                ->from('Users')
+                ->andWhere('Name','=','Hannsel')
+                ->get();
+    }
+
+    public function testAndwhereReturnsExpectedSqlStringIfWherenotIsSet()
+    {
+        $expected = "SELECT Name AS User FROM Users WHERE NOT User = 'Hannsel' AND City = 'Berlin'".";";
+        $query = $this->qb->select(['Name'])
+                        ->as('User')
+                        ->from('Users')
+                        ->whereNot('User','=','Hannsel')
+                        ->andWhere('City','=','Berlin')
+                        ->get();
+
+        $this->assertEquals($expected, $query);  
+    }
+
+    public function testAndwhereReturnsExpectedSqlString()
+    {
+        $expected = "SELECT Name AS User FROM Users WHERE User = 'Hannsel' AND City = 'Berlin'".";";
+        $query = $this->qb->select(['Name'])
+                        ->as('User')
+                        ->from('Users')
+                        ->where('User','=','Hannsel')
+                        ->andWhere('City','=','Berlin')
+                        ->get();
+
+        $this->assertEquals($expected, $query);  
+    }
+
+    public function testOrwhereReturnsExceptionIfQueryTypeIsNotSet()
+    {
+        $this->expectException(Exception::class);
+        $this->qb->as('Homeland')
+                ->from('Users')
+                ->where('City','=','Berlin')
+                ->orWhere('Name','=','Hannsel')
+                ->get();
+    }
+
+    public function testOrwhereReturnsExceptionIfQueryTypeIsWrong()
+    {
+        $this->expectException(Exception::class);
+        $this->qb->insert('Users', ['Germany', 'Berlin'])
+                ->as('Homeland')
+                ->from('Users')
+                ->where('City','=','Berlin')
+                ->orWhere('Name','=','Hannsel')
+                ->get();
+    }
+
+    public function testOrwhereReturnsExceptionIfWhereIsNotSet()
+    {
+        $this->expectException(Exception::class);
+        $this->qb->select(['Country'])
+                ->as('Homeland')
+                ->from('Users')
+                ->orWhere('Name','=','Hannsel')
+                ->get();
+    }
+
+    public function testOrwhereReturnsExpectedSqlStringIfWherenotIsSet()
+    {
+        $expected = "SELECT Name AS User FROM Users WHERE NOT User = 'Hannsel' AND City = 'Berlin'".";";
+        $query = $this->qb->select(['Name'])
+                        ->as('User')
+                        ->from('Users')
+                        ->whereNot('User','=','Hannsel')
+                        ->andWhere('City','=','Berlin')
+                        ->get();
+
+        $this->assertEquals($expected, $query);  
+    }
+
+    public function testOrwhereReturnsExpectedSqlString()
+    {
+        $expected = "SELECT Name AS User FROM Users WHERE User = 'Hannsel' OR City = 'Berlin'".";";
+        $query = $this->qb->select(['Name'])
+                        ->as('User')
+                        ->from('Users')
+                        ->where('User','=','Hannsel')
+                        ->orWhere('City','=','Berlin')
+                        ->get();
+
+        $this->assertEquals($expected, $query);  
+    }
+    public function testWherenotReturnsExceptionIfQueryTypeIsNotSet()
+    {
+        $this->expectException(Exception::class);
+        $this->qb->as('Homeland')
+                ->from('Users')
+                ->whereNot('City','=','Berlin')
+                ->get();
+    }
+
+    public function testWherenotReturnsExceptionIfQueryTypeIsWrong()
+    {
+        $this->expectException(Exception::class);
+        $this->qb->insert('Users', ['Germany', 'Berlin'])
+                ->as('Homeland')
+                ->from('Users')
+                ->whereNot('City','=','Berlin')
+                ->get();
+    }
+
+    public function testWherenotReturnsExceptionIfWhereIsSet()
+    {
+        $this->expectException(Exception::class);
+        $this->qb->select(['Country'])
+                ->as('Homeland')
+                ->from('Users')
+                ->where('Name','=','Hannsel')
+                ->whereNot('City','=','Berlin')
+                ->get();
+    }
+
+    public function testWherenotReturnsExpectedSqlString()
+    {
+        $expected = "SELECT Name AS User FROM Users WHERE NOT City = 'Berlin'".";";
+        $query = $this->qb->select(['Name'])
+                        ->as('User')
+                        ->from('Users')
+                        ->whereNot('City','=','Berlin')
+                        ->get();
+
+        $this->assertEquals($expected, $query);  
+    }   
+
+
 
 }
 
