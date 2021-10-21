dddbc3d202b98d28b7eca8a1a371ab3efe73848c
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-21 09:55:05 +0200

    NavBars Tests

---
+++ app/views/components/logged-navbar.php
@@ -0,0 +1,25 @@
+<div class="navbar-main-box">
+    <div id="logo-box">
+        <h2>DOGFRIENDLIST</h2>
+        <p>LOGGED NAVBAR</p>
+    </div>
+    <div id="logged-navbar-buttons-box">
+        <nav id="navbar-menu">
+            <ul>
+                <li><a href="spot/new-spot">+</a></li>
+                |
+                <li><a href="favourites">Favoritos</a></li>
+                |
+                <!-- inicio menu desplegable -->
+                <ul>
+                    <li><a href="user/profile">Mi perfil</a></li>
+                    <li><a href="user/personal-spots">Mis spots</a></li>
+                    <li><a href="user/config">Configuración del perfil</a></li>      
+                    <li><a href="auth/logout">Cerrar sesión</a></li>          
+                </ul>
+                 <!-- fin menu desplegable -->
+            </ul>
+
+        </nav>
+    </div>
+</div>
---
+++ app/views/components/unlogged-navbar.php
@@ -0,0 +1,12 @@
+
+<div class="navbar-main-box">
+    <div id="logo-box">
+        <h2>DOGFRIENDLIST</h2>
+        <p>UNLOGGED NAVBAR</p>
+    </div>
+    <div id="unlogged-navbar-buttons-box">
+        <a href="auth/login">Login</a>
+        |
+        <a href="auth/register">Registrarse</a>
+    </div>
+</div>
