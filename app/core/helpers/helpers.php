716d5e5dc03308a7ff4a04ab0949d7ddf3a0b5ea
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-21 09:53:29 +0200

    Creating helpers

---
+++ app/core/helpers/helpers.php
@@ -0,0 +1,23 @@
+<?php 
+
+function render(string $page, bool $base_page = true, array $params = null)
+{
+    if($base_page)
+    {          
+        if(isset($_SESSION)) {$header = COMPONENTS_PATH.'logged-navbar.php';}
+        else {$header = COMPONENTS_PATH.'unlogged-navbar.php';}     
+        
+        $main_content = BASE_VIEW_PATH.$page;
+        require_once \BASE_VIEW_TEMPLATE;
+    }
+    else
+    {
+        require_once \BASE_VIEW_PATH.$page;
+    }
+}
+
+function redirect(string $route)
+{
+    $root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].'/';
+    header("Location: $root.$route");
+}
