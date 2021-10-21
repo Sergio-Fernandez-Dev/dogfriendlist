67f86d3e17dc082e483ccf0cd5f8b24898926551
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-21 09:55:47 +0200

    Creating config file

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+define('BASE_VIEW_PATH', '../app/views/');
+define('BASE_VIEW_TEMPLATE', '../app/views/base-template.php');
+define('COMPONENTS_PATH', '../app/views/components/');
+
+
+
+require_once '../app/core/Router.php';
+require_once '../app/core/helpers/helpers.php';
+?>
