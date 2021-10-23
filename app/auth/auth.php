e097cf8754d51be8e56dccc2e05dba1487f4ac1e
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-23 15:16:58 +0200

    changing href

--- app/auth/auth.php
+++ app/auth/auth.php
@@ -1,34 +1,43 @@
 <?php 
 
-use App\Core\Interfaces\QueryBuilderInterface;
+
 use App\Core\QueryBuilder;
+use App\Core\DBHandler;
 use App\Core\Router as route;
+
 
 route::add('/login', function()
 {
-   return render('auth/login-form.php', true);
+   return render('auth/login-form.php', true, ['title' => 'Dogfriendlist - Login']);
 },
 'GET');
 
 route::add('/login-form',function()
 {
-    echo '<form action="login-form" method="post"><input type="text" name="test" /><input type="submit" value="send" /></form>';
+   echo '<form action="login-form" method="post"><input type="text" name="test" /><input type="submit" value="send" /></form>';
 },
 'POST');
 
 route::add('/register', function(){
 
-   return render('auth/register-form.php', true);
+   return render('auth/register-form.php', true, ['title' => 'Dogfriendlist - Registro']);
 },
 'GET');
 
 route::add('/register', function()
 {
+   $dbh = new DBHandler();
    $qb = new QueryBuilder();
    $form = new RegisterForm($_POST, $qb);
-   $query = $form->send();
+   $query = $form->send($dbh);
 },
 'POST');
+
+route::add('/all', function(){
+
+   return print_r(route::getAll());
+},
+'GET');
 
 route::run('/auth');
 
