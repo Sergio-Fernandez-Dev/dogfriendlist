47eef7ff14152478877568b7b8c543197b621010
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-23 15:18:20 +0200

    adding routes

--- config/config.php
+++ config/config.php
@@ -3,9 +3,12 @@
 define('BASE_VIEW_PATH', '../app/views/');
 define('BASE_VIEW_TEMPLATE', '../app/views/base-template.php');
 define('COMPONENTS_PATH', '../app/views/components/');
-
+define('DB_CONFIG', '../config/database.php');
 
 
 require_once '../app/core/Router.php';
 require_once '../app/core/helpers/helpers.php';
+require_once '../app/core/DBHandler.php';
+require_once '../app/auth/forms/RegisterForm.php'
+
 ?>
