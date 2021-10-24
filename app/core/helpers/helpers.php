95e323e734a2c7eb5e714025c7596f26917b540b
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-24 21:17:22 +0200

    Declaring $params

--- app/core/helpers/helpers.php
+++ app/core/helpers/helpers.php
@@ -2,6 +2,8 @@
 
 function render(string $page, bool $base_page = true, array $params = null)
 {
+    $params;
+
     if($base_page)
     {          
         if(isset($_SESSION)) {$header = COMPONENTS_PATH.'logged-navbar.php';}
@@ -14,6 +16,8 @@ function render(string $page, bool $base_page = true, array $params = null)
     {
         require_once \BASE_VIEW_PATH.$page;
     }
+
+    echo $_SERVER['REQUEST_METHOD'];
 }
 
 function redirect(string $route)
