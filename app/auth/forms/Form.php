abbfb7058e7955d79072ee2891b9fac8c9bfd5f0
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-24 21:13:54 +0200

    Forcing string type

--- app/auth/forms/Form.php
+++ app/auth/forms/Form.php
@@ -1,10 +1,20 @@
 <?php 
 
+namespace App\Auth\Forms;
+
+use App\Core\Interfaces\ConnectionInterface;
+
 abstract class Form
 {
-
-
+    /**
+     * Filtra los valores introducidos en los campos del formulario
+     *para evitar código malicioso.
+     * @param array $data
+     * 
+     * @return array
+     */
     private function _filter($data){
+        $data = (string) $data;
         $data = trim($data); // Elimina espacios antes y después de los data
         $data = stripslashes($data); // Elimina backslashes \
         $data = htmlspecialchars($data); // Traduce caracteres especiales en entidades HTML
@@ -19,14 +29,14 @@ abstract class Form
         {
             foreach($data as $key => $value)
             {
-                $filtered_data[$key] = $this->_filter($data[$key]);
+                $filtered_data[$key] = $this->_filter($value);
             }
         }
 
         return $filtered_data;
     }
 
-    abstract protected function _prepare();
+    abstract public function send(ConnectionInterface $dbh);
 }
 
 ?>
