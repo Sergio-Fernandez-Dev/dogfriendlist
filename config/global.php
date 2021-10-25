2a9d82497543e56f8fe69d1f85e9e680dd1992db
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-10-25 14:25:10 +0200

    Changing filename

---
+++ config/global.php
@@ -0,0 +1,8 @@
+<?php 
+
+define('BASE_VIEW_PATH', '../app/views/');
+define('BASE_VIEW_TEMPLATE', '../app/views/base-template.php');
+define('COMPONENTS_PATH', '../app/views/components/');
+define('DB_CONFIG', '../config/database.php');
+
+?>
