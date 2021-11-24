b7afa1837e21bdf808be9c850dfbd970bf5b8ab8
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-11-24 09:29:59 +0100

    Adding styles

--- app/views/components/logged-navbar.php
+++ app/views/components/logged-navbar.php
@@ -4,31 +4,38 @@ $profile_img = $user->getImg();
 ?>
 
 <header class="header">
-    <h1 class="header__logo">DOGFRIENDLIST</h1>
-    <nav class="nav">
-        <div class="nav__menu">
-            <div class="nav__button">
-                <a class="nav__link" href="../spot/new">
-                    <i class="material-icons large">add_location_alt</i>
-                </a>
+    <div class="header__content">
+        <h1 class="header__logo">DOGFRIENDLIST</h1>
+        <nav class="nav">
+            <div class="nav__menu">
+                <div class="nav__button">
+                    <a class="nav__link" href="../spot/new">
+                        <i class="material-icons">add_location_alt</i>
+                    </a>
+                </div>
+                <div class="nav__button">
+                    <a class="nav__link" href="../user/favourites">
+                        <span class="material-icons">favorite</span>
+                    </a>
+                </div>
             </div>
-            <div class="nav__button">
+        </nav>
+        <div class="profile-menu">
+            <div class="profile-menu__picture">
                 <a class="nav__link" href="../user/favourites">
-                    <span class="material-icons md-36">favorite</span>
+                    <span class="material-icons">account_circle</span>
+                    <span class="material-icons">expand_more</span>
                 </a>
             </div>
+                <!-- Menú desplegable -->
+            <div class="profile-menu__submenu">
+                <a class="nav__link nav__link--cascade" href="#">Mi perfil</a>
+                <a class="nav__link nav__link--cascade" href="#">Mis spots</a>
+                <a class="nav__link nav__link--cascade" href="../auth/logout">Cerrar sesión</a>
+            </div>
+                <!-- fin menu desplegable -->
         </div>
-    </nav>
-    <div class="header__profile">
-        <img src="<?php echo $profile_img; ?>">
     </div>
-        <!-- Menú desplegable -->
-    <div class="header__submenu">
-        <a class="nav__link nav__link--cascade" href="#">Mi perfil</a>
-        <a class="nav__link nav__link--cascade" href="#">Mis spots</a>
-        <a class="nav__link nav__link--cascade" href="../auth/logout">Cerrar sesión</a>
-    </div>
-         <!-- fin menu desplegable -->
 
 </header>
 
