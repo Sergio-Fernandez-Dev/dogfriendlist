651935a20bfd6600e12acd51df337b0a2b4105e4
zednanref <sr.sergiofernandezfernandez@gmail.com>
2021-11-29 21:55:19 +0100

    Charging scripts dinamically

--- config/global.php
+++ config/global.php
@@ -1,9 +1,20 @@
 <?php
 
+require '../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable('../');
+$dotenv->safeLoad();
+
 define('BASE_VIEW_PATH', '../app/views/');
 define('BASE_VIEW_TEMPLATE', '../app/views/base-template.php');
 define('COMPONENTS_PATH', '../app/views/components/');
+
 define('DB_CONFIG', '../config/database.php');
+
 define('MAIL_CONFIG', '../config/mail.php');
+
+define('API_KEY', $_ENV['API_KEY']);
+
+define('SCRIPTS_PATH', '../app/views/components/scripts/');
 
 ?>
