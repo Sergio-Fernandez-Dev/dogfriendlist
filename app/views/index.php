604e057f3b569e1551450e933924828c2608f854
zednanref <sr.sergiofernandezfernandez@gmail.com>
2022-02-01 18:03:10 +0100

    search with enter

--- app/views/index.php
+++ app/views/index.php
@@ -1,7 +1,8 @@
 <div class="box">
-    <div class="form form--finder-style" id="finder-form">
+<form class="form" id="new-spot-form" method="POST" onkeydown="return event.key != 'Enter';">
+    <div class="form--finder-style" id="finder-form">
         <input class="form__field form__field--finder-style" id="address" name="address"
-        type="text" placeholder="Buscando ubicación" onkeydown="checkEnterIsPressed(event)">
+        type="text" placeholder="Buscando ubicación" onkeydown="checkEnterIsPressed(event, 'index')">
         <select class="form__select form__select--finder-style" id="finder-category" name="category" placeholder="Todos los spots">
             <option class="form__option" selected value="1">Todos los spots</option>
             <option class="form__option" value="2">Parques y zonas verdes</option>
